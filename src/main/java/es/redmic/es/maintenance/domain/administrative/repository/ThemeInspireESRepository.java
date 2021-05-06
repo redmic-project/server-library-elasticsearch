ca55c18d9d19867aba07701a4a02abb7665956a9
Noel <noelalonso1986@gmail.com>
2021-05-06 15:14:17 +0100

    Sobrescribe método para obtener mapping de elastic

--- src/main/java/es/redmic/es/maintenance/domain/administrative/repository/ThemeInspireESRepository.java
+++ src/main/java/es/redmic/es/maintenance/domain/administrative/repository/ThemeInspireESRepository.java
@@ -1,5 +1,7 @@
 package es.redmic.es.maintenance.domain.administrative.repository;
 
+import org.springframework.stereotype.Repository;
+
 /*-
  * #%L
  * ElasticSearch
@@ -9,9 +11,9 @@ package es.redmic.es.maintenance.domain.administrative.repository;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,6 +25,7 @@ package es.redmic.es.maintenance.domain.administrative.repository;
 import es.redmic.es.data.common.repository.RWDataESRepository;
 import es.redmic.models.es.maintenance.administrative.model.ThemeInspire;
 
+@Repository
 public class ThemeInspireESRepository extends RWDataESRepository<ThemeInspire> {
 
 	private static String[] INDEX = { "theme-inspire" };
@@ -30,5 +33,10 @@ public class ThemeInspireESRepository extends RWDataESRepository<ThemeInspire> {
 
 	public ThemeInspireESRepository() {
 		super(INDEX, TYPE);
+	}
+
+	@Override
+	protected String getMappingFilePath(String index, String type) {
+		return MAPPING_BASE_PATH + "domains/" + INDEX + MAPPING_FILE_EXTENSION;
 	}
 }
--- src/main/java/es/redmic/es/maintenance/domain/administrative/service/ThemeInspireESService.java
+++ src/main/java/es/redmic/es/maintenance/domain/administrative/service/ThemeInspireESService.java
@@ -9,9 +9,9 @@ package es.redmic.es.maintenance.domain.administrative.service;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,6 +21,7 @@ package es.redmic.es.maintenance.domain.administrative.service;
  */
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import es.redmic.es.common.service.MetaDataESService;
 import es.redmic.es.maintenance.domain.administrative.repository.ThemeInspireESRepository;
@@ -28,6 +29,7 @@ import es.redmic.models.es.common.model.ReferencesES;
 import es.redmic.models.es.maintenance.administrative.dto.ThemeInspireDTO;
 import es.redmic.models.es.maintenance.administrative.model.ThemeInspire;
 
+@Service
 public class ThemeInspireESService extends MetaDataESService<ThemeInspire, ThemeInspireDTO> {
 
 	@Autowired
