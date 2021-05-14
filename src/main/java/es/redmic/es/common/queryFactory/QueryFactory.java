257c4faa665b0cab4c5de8321390c340d67dfb31
Noel <noelalonso1986@gmail.com>
2021-05-14 09:06:34 +0100

    Añade gestión de diferentes tipos de consultas

--- src/main/java/es/redmic/es/common/queryFactory/QueryFactory.java
+++ src/main/java/es/redmic/es/common/queryFactory/QueryFactory.java
@@ -23,6 +23,8 @@ package es.redmic.es.common.queryFactory;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 
@@ -30,9 +32,13 @@ import es.redmic.es.common.queryFactory.common.BaseQueryUtils;
 import es.redmic.exception.common.ExceptionType;
 import es.redmic.exception.common.InternalException;
 import es.redmic.models.es.common.query.dto.DataQueryDTO;
+import es.redmic.models.es.common.query.dto.GeoDataQueryDTO;
+import es.redmic.models.es.common.query.dto.MetadataQueryDTO;
 import es.redmic.models.es.common.query.dto.SimpleQueryDTO;
 
 public final class QueryFactory {
+
+	protected static Logger logger = LogManager.getLogger();
 
 	/*
 	 * GetQuery para todo tipo de repositorios
@@ -43,14 +49,32 @@ public final class QueryFactory {
 		try {
 			Class<? extends BaseQueryUtils> utils = QueryType.getUtils(queryDTO.getDataType());
 
-			Method method = utils.getDeclaredMethod("getQuery", DataQueryDTO.class, QueryBuilder.class,
+			Method method;
+
+			logger.debug("Obteniendo query para tipo {} - utils {}", queryDTO.getDataType(), utils.getSimpleName());
+
+			if (queryDTO instanceof GeoDataQueryDTO) {
+
+				method = utils.getDeclaredMethod("getQuery", GeoDataQueryDTO.class, QueryBuilder.class,
 					QueryBuilder.class);
+			}
+			else if (queryDTO instanceof MetadataQueryDTO) {
+
+				method = utils.getDeclaredMethod("getQuery", MetadataQueryDTO.class, QueryBuilder.class,
+					QueryBuilder.class);
+			}
+			else {
+
+				method = utils.getDeclaredMethod("getQuery", DataQueryDTO.class, QueryBuilder.class,
+					QueryBuilder.class);
+			}
 
 			Object[] params = new Object[] { queryDTO, internalQuery, partialQuery };
 
 			return (BoolQueryBuilder) method.invoke(utils, params);
 		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException
 				| SecurityException e) {
+			e.printStackTrace();
 			throw new InternalException(ExceptionType.INTERNAL_EXCEPTION, e);
 		}
 	}
