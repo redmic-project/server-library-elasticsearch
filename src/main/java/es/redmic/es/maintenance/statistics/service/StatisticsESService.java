db798a6ce96e5c59610d915a9bd0af5ae5b5a0d8
Noel <noelalonso1986@gmail.com>
2021-05-21 09:39:30 +0100

    Actualiza consultas de estadísticas

--- src/main/java/es/redmic/es/maintenance/statistics/service/StatisticsESService.java
+++ src/main/java/es/redmic/es/maintenance/statistics/service/StatisticsESService.java
@@ -9,9 +9,9 @@ package es.redmic.es.maintenance.statistics.service;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -46,7 +46,6 @@ public class StatisticsESService {
 		dto.setActivity(repository.activitiesStatistics());
 		dto.setActivityOutProject(repository.activityOutProjectStatistics());
 
-		dto.setContact(repository.contactsStatistics());
 		dto.setPlatform(repository.platformsStatistics());
 		dto.setDocument(repository.documentsStatistics());
 		dto.setOrganisation(repository.organisationStatistics());
