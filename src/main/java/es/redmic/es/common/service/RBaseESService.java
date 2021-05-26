23c677bd0ec014c01e807ba18e10a8cec2844a7b
Noel <noelalonso1986@gmail.com>
2021-05-26 12:39:16 +0100

    Controla parámetros no instanciables

--- src/main/java/es/redmic/es/common/service/RBaseESService.java
+++ src/main/java/es/redmic/es/common/service/RBaseESService.java
@@ -9,9 +9,9 @@ package es.redmic.es.common.service;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -71,11 +71,15 @@ public abstract class RBaseESService<TModel extends BaseES<?>, TDTO extends Base
 	@SuppressWarnings("unchecked")
 	public RBaseESService() {
 
-		this.typeOfTModel = (Class<TModel>) (((ParameterizedType) getClass().getGenericSuperclass())
+		if (getClass().getGenericSuperclass() instanceof ParameterizedType) {
+			this.typeOfTModel = (Class<TModel>) (((ParameterizedType) getClass().getGenericSuperclass())
 				.getActualTypeArguments()[0]);
+		}
 
-		this.typeOfTDTO = (Class<TDTO>) (((ParameterizedType) getClass().getGenericSuperclass())
+		if (getClass().getGenericSuperclass() instanceof ParameterizedType) {
+			this.typeOfTDTO = (Class<TDTO>) (((ParameterizedType) getClass().getGenericSuperclass())
 				.getActualTypeArguments()[1]);
+		}
 	}
 
 	@SuppressWarnings("unchecked")
